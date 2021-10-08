3c89c73b105eedd646797c8b597d2c46d8824984
Ditto Alvarez <dittoalvarez@gmail.com>
2021-10-09 01:37:36 +0800

    tried building with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:heart_token/constants.dart';
+import 'screens/welcome/welcome_screen.dart';
 
-void main() {}
+void main() => runApp(MyApp());
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:false,
+      title: 'Heart Token',
+      theme: ThemeData(
+        primaryColor: kprimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen()
+    )
+  }
+}
---
+++ lib/screens/welcome/components/body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+        height: size.height,
+        width: double.infinity,
+        child: Stack(
+          alignment: Alignment.center,
+        ));
+  }
+}
